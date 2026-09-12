04a05e2a8b78ef3b007af9a5d8cb6975e98b21ff
tsuduratsuki <tdr72tsuki126@gmail.com>
2026-09-13 05:27:36 +0900

    Add files via upload

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,72 +1,28 @@
+const { REST, Routes } = require("discord.js");
+const fs = require("fs");
 require("dotenv").config();
-const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 
-// ★ あなたの GAS API URL を入れてね
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwReLt9RQ98jXaUFPFbtOt5dbpq6zgmTeMnEa4xQnFbR57G1xJDvcYmUh45tvq4VO-m/exec";
+const commands = [];
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 
-// Node18 の標準 fetch
-async function getWeapons() {
-  const res = await fetch(GAS_URL);
-  return await res.json();
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
 }
 
-async function main() {
-  console.log("GAS から武器一覧を取得中…");
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
-  const weapons = await getWeapons();
-
-  // ⭐ 武器種・サブ・スペシャルの一覧を自動生成
-  const types = [...new Set(weapons.map(w => w.type))];
-  const subs = [...new Set(weapons.map(w => w.sub))];
-  const specials = [...new Set(weapons.map(w => w.special))];
-
-  console.log("抽選用データ生成完了");
-
-  // ⭐ /solo のコマンドを自動生成
-  const soloCommand = new SlashCommandBuilder()
-    .setName("solo")
-    .setDescription("ソロ武器抽選を開始します")
-    .addStringOption(option =>
-      option
-        .setName("mode")
-        .setDescription("抽選方法を選んでください")
-        .setRequired(true)
-        .addChoices(
-          { name: "通常（完全ランダム）", value: "normal" },
-          { name: "武器種限定", value: "type" },
-          { name: "サブ限定", value: "sub" },
-          { name: "スペシャル限定", value: "special" }
-        )
-    )
-    .addStringOption(option => {
-      option
-        .setName("filter")
-        .setDescription("武器種・サブ・スペシャルを選択（限定抽選の場合）")
-        .setRequired(false);
-
-      // ⭐ ここで choices を自動生成
-      types.forEach(t => option.addChoices({ name: `武器種：${t}`, value: t }));
-      subs.forEach(s => option.addChoices({ name: `サブ：${s}`, value: s }));
-      specials.forEach(sp => option.addChoices({ name: `スペシャル：${sp}`, value: sp }));
-
-      return option;
-    });
-
-  const commands = [soloCommand.toJSON()];
-
-  // ⭐ Discord に登録
-  const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
-
+(async () => {
   try {
-    console.log("Discord にコマンドを登録中…");
+    console.log("Started refreshing global application (/) commands.");
+
     await rest.put(
       Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands }
     );
-    console.log("登録完了！");
-  } catch (err) {
-    console.error(err);
-  }
-}
 
-main();
+    console.log("Successfully reloaded global application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
