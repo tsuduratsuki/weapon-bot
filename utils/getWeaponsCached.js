fec492bf37df1b035d99f9e8b8a659bb415ec40a
tsuduratsuki <tdr72tsuki126@gmail.com>
2026-09-12 23:31:32 +0900

    Add files via upload

--- utils/getWeaponsCached.js
+++ utils/getWeaponsCached.js
@@ -1,1 +1,23 @@
+const fetch = require("node-fetch");
 
+let cachedWeapons = null;
+let lastFetchTime = 0;
+
+async function getWeaponsCached() {
+  const now = Date.now();
+
+  // 10分キャッシュ
+  if (cachedWeapons && now - lastFetchTime < 10 * 60 * 1000) {
+    return cachedWeapons;
+  }
+
+  const res = await fetch(process.env.GAS_URL);
+  const data = await res.json();
+
+  cachedWeapons = data;
+  lastFetchTime = now;
+
+  return data;
+}
+
+module.exports = { getWeaponsCached };
