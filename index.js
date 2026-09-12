d328d9d611ada6f1ab8d729f6f0278536c943cfb
tsuduratsuki <tdr72tsuki126@gmail.com>
2026-09-12 20:48:05 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const fs = require("fs");
 const path = require("path");
 const { Client, GatewayIntentBits, ActionRowBuilder, StringSelectMenuBuilder } = require("discord.js");
