db51d8ef0fbe39a740d1409e6677ff49688035e4
tsuduratsuki <tdr72tsuki126@gmail.com>
2026-09-20 19:16:39 +0900

    Add files via upload

--- commands/team4.js
+++ commands/team4.js
@@ -7,9 +7,12 @@ const {
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("team4")
-    .setDescription("4人用武器抽選（オープン）"),
+    .setDescription("4人用武器抽選（プラベ）"),
 
   async execute(interaction) {
+
+    // ★ まず必ず deferReply（ephemeral なし）
+    await interaction.deferReply();
 
     const modeMenu = new StringSelectMenuBuilder()
       .setCustomId("team4Mode_v2")
@@ -23,10 +26,10 @@ module.exports = {
 
     const row = new ActionRowBuilder().addComponents(modeMenu);
 
-    return interaction.reply({
+    // ★ editReply で確実に「reply」を送る
+    return interaction.editReply({
       content: "抽選方法を選んでください：",
       components: [row]
     });
   }
 };
-
